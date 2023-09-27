ec6418ca8952ee4b70576d55429611511af47aff
externalsip <93773873+externalsip@users.noreply.github.com>
2023-09-27 16:41:04 -0400

    Started actuality section + other stuff
    -Imported lato font
    -Made a partial containing all the colors for the website as scss variables
    -Added swipers for the images and text of actuality (was not tested)
    -Changed a few things in the style of hero and project sections.

--- website/script.js
+++ website/script.js
@@ -2,7 +2,7 @@
 swiper in the hero uses swipe.js, this is the code that makes said swiper work.
 */
 
-const swiper = new Swiper('.swiper', {
+const swiperPro = new Swiper('.swiperProject', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -19,3 +19,21 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
     }
   });
+
+const swiperAct1 = new Swiper('.swiperActIMG', {
+  direction:"horizontal",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "fade",
+  loop:true,
+});
+const swiperAct2 = new Swiper('.swiperActTXT', {
+  direction:"horizontal",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop:true,
+});
